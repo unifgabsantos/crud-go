fa418ed019c9fafc91bee1ea127ebc48c6d740f9
Gabriel Lopes Parreira dos Santos <gabriel.lopessb@gmail.com>
2025-09-18 22:42:18 -0300

    Update routes.go

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -2,12 +2,13 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/unifgabsantos/crud-go/src/controller"
 )
 
-func InitializeRoutes(r *gin.RouterGroup) {
-	r.GET("/User/:id")
-	r.GET("/User/:email")
-	r.POST("/User")
-	r.PUT("/User/:id")
-	r.DELETE("/User/:id")
+func InitRoutes(r *gin.RouterGroup) {
+	r.GET("/User/:id", controller.FindUserById)
+	r.GET("/User/:email", controller.FindUserByEmail)
+	r.POST("/User", controller.CreateUser)
+	r.PUT("/User/:id", controller.UpdateUser)
+	r.DELETE("/User/:id", controller.DeleteUser)
 }
