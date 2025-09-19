e408e55c323f0ddbb6e17ae5b2c6749ad0d9a2ce
Gabriel Lopes Parreira dos Santos <gabriel.lopessb@gmail.com>
2025-09-19 17:42:18 -0300

    Update user_request.go

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -2,7 +2,7 @@ package request
 
 type UserRequest struct {
 	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
-	Name     string `json:"name" binding:"required"`
-	Age      int8   `json:"age" binding:"required,min=0"`
+	Password string `json:"password" binding:"required,min=8"`
+	Name     string `json:"name" binding:"required,min=2,max=100"`
+	Age      int8   `json:"age" binding:"required,min=1,max=130"`
 }
