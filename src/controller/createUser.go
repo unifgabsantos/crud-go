5762c4e088c4dad42f09e6d80e07df98cf897f0a
Gabriel Lopes Parreira dos Santos <gabriel.lopessb@gmail.com>
2025-09-19 17:07:24 -0300

    Update createUser.go

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,21 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/unifgabsantos/crud-go/src/configuration/rest_err"
+	"github.com/unifgabsantos/crud-go/src/controller/model/request"
+)
 
 func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
 
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("invalid json body: %s", err.Error()))
+		c.JSON(int(restErr.Code), restErr)
+		return
+	}
+	fmt.Println(userRequest)
 }
